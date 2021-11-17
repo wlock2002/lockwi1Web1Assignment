99277a8b10a9d9ce7d61ed15e1f7de14aedc0ec7
wlock2002 <william.lock@gmail.com>
2021-11-17 20:43:55 +1300

    kinda working buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,13 @@
 
 //Main links using Ajax and Jquery to make the text function as buttons. 
 $(document).ready(function(e) {   
+    //Home
     $("#container").click(function(){
         disappear();
         document.getElementById('container1').setAttribute("style","transform:scale(1)");
 
     });
+    //Main tabs
     $("#vehicles").click(function(){
         shrink();
         document.getElementById('container2').setAttribute("style","display:block");
@@ -25,6 +27,22 @@ $(document).ready(function(e) {
     $("#about").click(function(){
         shrink();
         document.getElementById('container4').setAttribute("style","display:block");
+    });
+    //Individual vehicles
+    $("#altezza").click(function(){
+        document.getElementById('altezzaInfo').setAttribute("style","display:block");
+        document.getElementById('hiluxInfo').setAttribute("style","display:none");
+        document.getElementById('chaserInfo').setAttribute("style","display:none");
+    });
+    $("#hilux").click(function(){
+        document.getElementById('hiluxInfo').setAttribute("style","display:block");
+        document.getElementById('altezzaInfo').setAttribute("style","display:none");
+        document.getElementById('chaserInfo').setAttribute("style","display:none");
+    });
+    $("#chaser").click(function(){
+        document.getElementById('chaserInfo').setAttribute("style","display:block");
+        document.getElementById('hiluxInfo').setAttribute("style","display:none");
+        document.getElementById('altezzaInfo').setAttribute("style","display:none");
     });
 
 
@@ -58,24 +76,24 @@ $(document).ready(function(e) {
 //     document.getElementById('container4').setAttribute("style","display:block");
 // }
 
-function altezza()
-{
-    document.getElementById('altezzaInfo').setAttribute("style","display:block");
-    document.getElementById('hiluxInfo').setAttribute("style","display:none");
-    document.getElementById('chaserInfo').setAttribute("style","display:none");
-}
-function hilux()
-{
-    document.getElementById('hiluxInfo').setAttribute("style","display:block");
-    document.getElementById('altezzaInfo').setAttribute("style","display:none");
-    document.getElementById('chaserInfo').setAttribute("style","display:none");
-}
-function chaser()
-{
-    document.getElementById('chaserInfo').setAttribute("style","display:block");
-    document.getElementById('hiluxInfo').setAttribute("style","display:none");
-    document.getElementById('altezzaInfo').setAttribute("style","display:none");
-}
+// function altezza()
+// {
+//     document.getElementById('altezzaInfo').setAttribute("style","display:block");
+//     document.getElementById('hiluxInfo').setAttribute("style","display:none");
+//     document.getElementById('chaserInfo').setAttribute("style","display:none");
+// }
+// function hilux()
+// {
+//     document.getElementById('hiluxInfo').setAttribute("style","display:block");
+//     document.getElementById('altezzaInfo').setAttribute("style","display:none");
+//     document.getElementById('chaserInfo').setAttribute("style","display:none");
+// }
+// function chaser()
+// {
+//     document.getElementById('chaserInfo').setAttribute("style","display:block");
+//     document.getElementById('hiluxInfo').setAttribute("style","display:none");
+//     document.getElementById('altezzaInfo').setAttribute("style","display:none");
+// }
 
 function shrink()
 {   
